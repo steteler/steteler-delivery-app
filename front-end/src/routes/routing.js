224c6933ced18d97d82b7822750f52488916a2b8
Rodrigo Nunes <brksnunes@protonmail.com>
2023-02-27 22:26:36 -0300

    feat: :sparkles: Routing
    Added new file to contain all routes for the app

---
+++ front-end/src/routes/routing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Register from '../pages/Register';
+
+export default function Routing() {
+  return (
+    <Routes>
+      {/* <Route path="/login" element={ <Login /> } /> */}
+      <Route path="/register" element={ <Register /> } />
+    </Routes>
+  );
+}
