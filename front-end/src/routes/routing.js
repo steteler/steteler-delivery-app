582a8592a3536a2c4fd63c70db44a3dc4ffcff96
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-01 17:42:43 -0300

    refactor: Routing
    Added necessary code to correctly redirect '/' to '/login'

--- front-end/src/routes/routing.js
+++ front-end/src/routes/routing.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route, Redirect } from 'react-router-dom';
 import Register from '../pages/Register';
 
 export default function Routing() {
   return (
     <Routes>
-      {/* <Route path="/" element={ <Login /> } />
+      {/* <Route path="/" />
+      <Route exact path="/">
+        <Redirect to="/login" />
+      </Route>
       <Route path="/login" element={ <Login /> } /> */}
       <Route path="/register" element={ <Register /> } />
     </Routes>
