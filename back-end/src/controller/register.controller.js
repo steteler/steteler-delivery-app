15b64d9e45d08a82fcf4eca505640151dcf3692a
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-03 18:29:49 -0300

    refactor: :art: Register Controller
    Changed the return structure of the Register Controller

--- back-end/src/controller/register.controller.js
+++ back-end/src/controller/register.controller.js
@@ -2,13 +2,15 @@ const { newRegisterService } = require('../service/register.service');
 
 const newRegisterController = async (req, res) => {
   const { name, email, password, role } = req.body;
-  try { 
-    const retorno = await newRegisterService(name, email, password, role);
-      
-    return res.status(201).json(retorno);
-  } catch (error) {
-    res.status(409).json(error);
-  }
+  const { type, message } = await newRegisterService(name, email, password, role);
+  if (type) return res.status(type).json({ message });
+  return res.status(201).json({ ...message });
+  // try { 
+  //   const retorno = await newRegisterService(name, email, password, role);
+  //   return res.status(201).json(retorno);
+  // } catch (error) {
+  //   res.status(409).json(error);
+  // }
 };
 
 module.exports = {
