0b135b5a5ca923553e2edecd5ad55f2b7e6bbb42
Raquel_Gomes <raquel.wurzler@hotmail.com>
2023-03-03 16:42:38 -0300

    feat: Renderiza componente TableFinalizaOrde no componente FinalizaOrder, Adiciona corpo da tabela com seus data-testids no componente TableFinalizaOrder

--- front-end/src/components/FinalizeOrder.jsx
+++ front-end/src/components/FinalizeOrder.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import TableFinalizaOrder from './TableFinalizaOrder';
 
 export default function FinalizaOrder() {
   return (
     <section>
       <h1>Finalizar Pedido</h1>
-      <section>1</section>
+      <section>
+        <TableFinalizaOrder />
+        {' '}
+        {/* Tem que enviar por props os itens que estão nesse carrinho de compras */}
+      </section>
     </section>
   );
 }
--- front-end/src/components/TableFinalizaOrder.jsx
+++ front-end/src/components/TableFinalizaOrder.jsx
@@ -1,6 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function TableFinalizaOrder() {
+/* Nos datatestids do tbody é pra usar index e não id, verificar */
+
+export default function TableFinalizaOrder({ listItens }) {
+  const tbody = listItens.map((exp) => {
+    const { id, descricao, quantidade, valorUnit } = exp;
+    return (
+      <tr key={ id }>
+        <td
+          data-testid={ `customer_checkout__element-order-table-item-number-${id}` }
+        >
+          {id}
+        </td>
+        <td
+          data-testid={ `customer_checkout__element-order-table-name-${id}` }
+        >
+          {descricao}
+        </td>
+        <td
+          data-testid={ `customer_checkout__element-order-table-quantity-${id}` }
+        >
+          {quantidade}
+
+        </td>
+        <td
+          data-testid={ `customer_checkout__element-order-table-unit-price-${id}` }
+        >
+          {valorUnit}
+        </td>
+        <td
+          data-testid={ `customer_checkout__element-order-table-sub-total-${id}` }
+        >
+          { /* Subtotal */ }
+        </td>
+        <td>
+          <button
+            type="button"
+            onClick={ () => this.btnDelete(id) }
+            data-testid={ `customer_checkout__element-order-table-remove-${id}` }
+          >
+            Remover
+          </button>
+        </td>
+      </tr>
+    );
+  });
   return (
     <table>
       <thead>
@@ -19,3 +64,12 @@ export default function TableFinalizaOrder() {
     </table>
   );
 }
+
+TableFinalizaOrder.propTypes = {
+  listItens: PropTypes.shape({
+    item: PropTypes.number,
+    descricao: PropTypes.string,
+    quantidade: PropTypes.number,
+    valorUnit: PropTypes.number,
+  }).isRequired,
+};
