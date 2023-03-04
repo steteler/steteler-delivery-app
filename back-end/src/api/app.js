09b12a3f5c28eea7c8140e718fe3418a53debf1a
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-04 15:02:34 -0300

    fix: :rotating_light: Lint
    Fixed linter errors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors({
   origin: 'http://localhost:3000',
   methods: ['GET', 'POST'],
-  credentials: true
+  credentials: true,
 }));
 
 app.use('/login', loginRouter);
