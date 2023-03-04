39243d76790c45c547809bda0b397aa86ae99bb4
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-03 22:31:05 -0300

    feat: :sparkles: App back-end
    Added CORS configuration to back-end/app.js

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const loginRouter = require('../routes/login.route');
 const registerRouter = require('../routes/register.route');
@@ -6,6 +7,12 @@ const registerRouter = require('../routes/register.route');
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
