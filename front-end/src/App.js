bec2fb1c70210317ca4fc1d7329af41ca749a2ff
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-05 14:42:31 -0300

    refactor: :fire: removed react query

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import './App.css';
 import Routing from './routes/routing';
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
-    <QueryClientProvider client={ queryClient }>
-      <BrowserRouter>
-        <Routing />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <Routing />
+    </BrowserRouter>
   );
 }
 
