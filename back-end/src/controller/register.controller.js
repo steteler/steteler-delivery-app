e8b77059e26f969c18cc70ba400d7d9e15316180
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-05 00:35:51 -0300

    chore: pfv eu nao aguento mais
    dor

--- back-end/src/controller/register.controller.js
+++ back-end/src/controller/register.controller.js
@@ -2,11 +2,11 @@ const { newRegisterService } = require('../service/register.service');
 // const { generateToken } = require('../helpers/JWTUtils');
 
 const newRegisterController = async (req, res) => {
-  const { name, email, password, role } = req.body;
-  const { type, message } = await newRegisterService(name, email, password, role);
+  const { name, email, password } = req.body;
+  const { type, message } = await newRegisterService(name, email, password);
   if (type) return res.status(type).json({ message });
   // const token = generateToken(message);
-  return res.status(201).json({ ...message });
+  return res.status(201).json(message);
 };
 
 module.exports = {
