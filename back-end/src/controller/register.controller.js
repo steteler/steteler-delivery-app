c2e2b5becab67304962809bef40a3a768ecbc34c
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-05 00:02:39 -0300

    fix: AAAAAAAAAAAAAAAAAA

--- back-end/src/controller/register.controller.js
+++ back-end/src/controller/register.controller.js
@@ -1,12 +1,12 @@
 const { newRegisterService } = require('../service/register.service');
-const { generateToken } = require('../helpers/JWTUtils');
+// const { generateToken } = require('../helpers/JWTUtils');
 
 const newRegisterController = async (req, res) => {
   const { name, email, password, role } = req.body;
   const { type, message } = await newRegisterService(name, email, password, role);
   if (type) return res.status(type).json({ message });
-  const token = generateToken(message);
-  return res.status(201).json({ ...message, token });
+  // const token = generateToken(message);
+  return res.status(201).json({ ...message });
 };
 
 module.exports = {
