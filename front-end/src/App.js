951cccf17e686fa72b3f2682304c30083eda02d9
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-06 12:25:31 -0300

    feat: :heavy_plus_sign: React Query
    Re-added react query as dependency

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import './App.css';
 import Routing from './routes/routing';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routing />
-    </BrowserRouter>
+    <QueryClientProvider client={ queryClient }>
+      <BrowserRouter>
+        <Routing />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
