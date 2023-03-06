4c9a4babb25d61998ec0165478311cb820fb33f4
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-06 15:52:07 -0300

    refactor: :art: Register Controller
    Refactored the controller rsponse

--- back-end/src/controller/register.controller.js
+++ back-end/src/controller/register.controller.js
@@ -1,12 +1,12 @@
 const { newRegisterService } = require('../service/register.service');
-// const { generateToken } = require('../helpers/JWTUtils');
+const { generateToken } = require('../helpers/JWTUtils');
 
 const newRegisterController = async (req, res) => {
   const { name, email, password } = req.body;
   const { type, message } = await newRegisterService(name, email, password);
   if (type) return res.status(type).json({ message });
-  // const token = generateToken(message);
-  return res.status(201).json(message);
+  const token = generateToken(message);
+  return res.status(201).json({ ...message, token });
 };
 
 module.exports = {
