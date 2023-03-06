1d925198a550747f7cb51fd9a324437f4733abaa
Raquel_Gomes <raquel.wurzler@hotmail.com>
2023-03-06 13:08:51 -0300

    feat: Renderiza o componente DetailsAndAddress no componente Checkout

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import DetailsAndAddress from '../components/DetailsAndAddress';
 import FinalizeOrder from '../components/FinalizeOrder';
 
 export default function Checkout() {
   return (
     <section>
       <FinalizeOrder />
+      <DetailsAndAddress />
     </section>
   );
 }
