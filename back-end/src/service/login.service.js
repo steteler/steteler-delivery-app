99aee71489ac261bf6048885e638ca11b78e0263
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-06 16:22:45 -0300

    refactor: :art: Login Service
    Refactored the logic of the login service layer

--- back-end/src/service/login.service.js
+++ back-end/src/service/login.service.js
@@ -1,17 +1,29 @@
+const md5 = require('md5');
 const { User } = require('../database/models/index');
 
-async function getByEmail({ email, password }) {
+const md5Crypt = (param) => md5(param);
+
+async function getByEmail(email) {
   const userFound = await User.findOne({
-    where: { email, password },
+    where: { email },
   });
 
-  if (!userFound) {
-    return { httpCode: 404, message: 'Not found' };
-  }
+  return userFound;
+};
 
-  return { httpCode: 200, message: userFound };
+async function login (email, password) {
+  const getUser = await getByEmail(email);
+  if (!getUser) return { type: 404, message: 'User not found' };
+
+  const incomingPasswd = md5Crypt(password);
+  const userPasswd = getUser.dataValues.password;
+
+  if (incomingPasswd !== userPasswd) return { type: 404, message: 'Incorrect password' };
+
+  const { id: _i, password: _p, ...userData } = getUser.dataValues;
+  return { type: null, message: userData };
 }
 
 module.exports = {
-  getByEmail,
+  login,
 };
