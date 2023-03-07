79acfc4e3cbc493c9ef4a883acbdf7994fa6ebad
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-07 11:49:48 -0300

    refactor: Products page
    reverted changes

--- front-end/src/pages/Customer/Products/Products.jsx
+++ front-end/src/pages/Customer/Products/Products.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Navbar from '../../../components/common/Navbar/Navbar';
 
 export default function Products() {
   return (
     <div>
-      <Navbar />
       <h1>Products</h1>
     </div>
   );
