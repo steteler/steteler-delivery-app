0beda9d2fe99eeb793d87e48ceb17ac38b54e036
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-07 00:11:00 -0300

    feat: :sparkles: Navbar
    added new component for the page navbar

---
+++ front-end/src/components/common/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import useLocalStorage from '../../hooks/useLocalStorage';
+
+export default function Navbar() {
+  const navigate = useNavigate();
+  const [storedValue] = useLocalStorage('user');
+  console.log(storedValue.name);
+
+  const btnHandler = () => {
+    localStorage.clear();
+    navigate('/login');
+  };
+  return (
+    <div>
+      <nav>
+        <h3 data-testid="customer_products__element-navbar-link-products">
+          PRODUTOS
+        </h3>
+        <h3 data-testid="customer_products__element-navbar-link-orders">
+          MEUS PEDIDOS
+        </h3>
+        <h2 data-testid="customer_products__element-navbar-user-full-name">
+          {storedValue.name}
+        </h2>
+        <button
+          type="button"
+          data-testid="customer_products__element-navbar-link-logout"
+          onClick={ btnHandler }
+        >
+          Sair
+        </button>
+
+      </nav>
+    </div>
+  );
+}
--- front-end/src/pages/Customer/Products/Products.jsx
+++ front-end/src/pages/Customer/Products/Products.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../../../components/common/Navbar';
 
 export default function Products() {
   return (
     <div>
+      <Navbar />
       <h1>Products</h1>
     </div>
   );
