5a7fd6c2b5c77cb88ae10c5a2de3b8724ec96c99
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-08 00:20:47 -0300

    feat: :sparkles: back-end/Middleware
    added new middleware to validate incoming Auth header tokens

---
+++ back-end/src/middlewares/tokenValidator.js
@@ -0,0 +1,16 @@
+const { validateToken } = require('../helpers/JWTUtils');
+
+const tokenValidator = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ message: 'Token not found' });
+  try {
+    const { data: { role } } = validateToken(token);
+    console.warn(role);
+    if(role !== 'administrator') return res.status(401).json({ message: 'Unauthorized' });
+    next();
+  } catch (err) {
+   return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+}
+
+module.exports = tokenValidator;
