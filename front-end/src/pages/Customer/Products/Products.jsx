4181db01c1aff36aa43b285ede57cf8147e7e68b
Raquel_Gomes <raquel.wurzler@hotmail.com>
2023-03-07 22:11:25 -0300

    refactor: Tentando resolver o requisito 15

--- front-end/src/pages/Customer/Products/Products.jsx
+++ front-end/src/pages/Customer/Products/Products.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import getProducts from '../../../api/getProducts';
 import ProductCard from '../../../components/ProductCard';
 
@@ -18,6 +19,15 @@ export default function Products() {
     <div>
       <h1>Products</h1>
       <ProductCard products={ products } />
+      <Link to="/customer/checkout">
+        <button
+          type="button"
+          data-testid="customer_products__button-cart"
+        >
+          <div data-testid="customer_products__checkout-bottom-value" />
+          total
+        </button>
+      </Link>
     </div>
   );
 }
