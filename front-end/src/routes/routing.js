66c915da9e5152594e85284d7a1b5c19a15e8a96
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-11 23:47:33 -0300

    fix: :bug: Routing
    Fix Structure

--- front-end/src/routes/routing.js
+++ front-end/src/routes/routing.js
@@ -4,12 +4,11 @@ import Register from '../pages/Register';
 import Login from '../pages/Login';
 import Products from '../pages/Customer/Products/Products';
 
-import Orders from '../pages/Customer/Orders/Orders';
-// import OrdersDetails from '../pages/Customer/Orders/OrdersDetails';
-
+// import Orders from '../pages/Customer/Orders/Orders';
 import Checkout from '../pages/Checkout';
 import Management from '../pages/Admin/Management';
 import OrdersDetails from '../pages/Orders/OrdersDetails';
+import CustomerOrders from '../pages/Customer/Orders/CustomerOrders';
 
 export default function Routing() {
   return (
@@ -18,7 +17,7 @@ export default function Routing() {
       <Route path="/login" element={ <Login /> } />
       <Route path="/register" element={ <Register /> } />
       <Route path="/customer/products" element={ <Products /> } />
-      <Route path="/customer/orders" element={ <Orders /> } />
+      <Route path="/customer/orders" element={ <CustomerOrders /> } />
       <Route path="/customer/checkout" element={ <Checkout /> } />
       <Route path="/customer/orders/:id" element={ <OrdersDetails /> } />
       <Route path="/admin/manage" element={ <Management /> } />
