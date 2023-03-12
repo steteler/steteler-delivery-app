647aa081dfe0b8a63f64a959c6dc57ae89a98258
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-11 23:48:45 -0300

    feat: :sparkles: user orders
    added new necessary methods to get all orders for a user

--- back-end/src/controller/seller/orders.controller.js
+++ back-end/src/controller/seller/orders.controller.js
@@ -1,12 +1,19 @@
 const {
   findAllSalesService,
   newSaleService,
+  getSaleByUserId,
 } = require('../../service/seller/orders.service');
 
 const findAllSalesController = async (req, res) => {
   const sales = await findAllSalesService();
   return res.status(200).json(sales);
 };
+
+const findSaleByUserId = async (req, res) => {
+  const { id } = req.params;
+  const sale = await getSaleByUserId(id);
+  return res.status(200).json(sale);
+}
 
 const newSaleController = async (req, res) => {
   const { type, message } = await newSaleService(req.body);
@@ -18,4 +25,5 @@ const newSaleController = async (req, res) => {
 module.exports = {
   findAllSalesController,
   newSaleController,
+  findSaleByUserId,
 };
