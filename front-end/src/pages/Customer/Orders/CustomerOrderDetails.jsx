b8e2140f187a2a397691ec612aa2662476c4f042
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-12 21:03:26 -0300

    feat: :sparkles: Customer Order Details
    Added navbar to page

--- front-end/src/pages/Customer/Orders/CustomerOrderDetails.jsx
+++ front-end/src/pages/Customer/Orders/CustomerOrderDetails.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Navbar from '../../../components/common/Navbar/Navbar';
 
 export default function CustomerOrderDetails() {
   const { id } = useParams();
   return (
     <div>
+      <Navbar />
       <h1>OrdersDetails</h1>
       <h2>
         { id }
