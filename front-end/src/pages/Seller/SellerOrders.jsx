7ea3208d9ceb9639fdf54b8e7d759cf427a683e8
Rodrigo Nunes <brksnunes@protonmail.com>
2023-03-12 21:04:03 -0300

    feat: :sparkles: Seller Orders
    Added navbar to page

--- front-end/src/pages/Seller/SellerOrders.jsx
+++ front-end/src/pages/Seller/SellerOrders.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import getOrders from '../../api/Seller/getOrders';
+import Navbar from '../../components/common/Navbar/Navbar';
 import SellerOrdersCard from '../../components/Seller/SellerOrdersCard';
-// import Navbar from '../../components/common/Navbar/Navbar';
 
 export default function SellerOrders() {
   const [orders, setOrders] = useState([]);
 
   const getAllOrders = async () => {
     const allOrders = await getOrders();
-    console.log(allOrders.data);
     return setOrders(allOrders.data);
   };
 
@@ -17,13 +16,9 @@ export default function SellerOrders() {
   }, []);
 
   return (
-    <div>
-      {/* <Navbar /> */}
+    <main>
+      <Navbar />
       <SellerOrdersCard orders={ orders } />
-      <h1>Página do vendedor</h1>
-      <button type="button" onClick={ () => getAllOrders() }>clique</button>
-      <button type="button" onClick={ () => console.log(orders) }>orders</button>
-      {/* <OrdersCard orders={ orders } /> */}
-    </div>
+    </main>
   );
 }
