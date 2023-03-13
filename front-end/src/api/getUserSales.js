dfd3d9afd947e76e2d203ac226aab2d1d7ec1b65
Raquel_Gomes <raquel.wurzler@hotmail.com>
2023-03-13 15:16:26 -0300

    feat: Tentando requisito 25 e 26

--- front-end/src/api/getUserSales.js
+++ front-end/src/api/getUserSales.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default async function getUserSales(id) {
-  const { data } = await axios.get(`http://localhost:3001/customer/orders/${id}`);
+  const { data } = await axios.get(`http://localhost:3001/customer/allOrders/${id}`);
   return data;
 }
--- front-end/src/pages/Customer/Orders/CustomerOrderDetails.jsx
+++ front-end/src/pages/Customer/Orders/CustomerOrderDetails.jsx
@@ -1,17 +1,107 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+// import { format } from 'date-fns';
 import Navbar from '../../../components/common/Navbar/Navbar';
+import getSellerId from '../../../api/getSellerId';
 
 export default function CustomerOrderDetails() {
   const { id } = useParams();
+
+  const {
+    isSuccess,
+    data: details,
+  } = useQuery('saleDetails', () => getSellerId(id));
+
   return (
-    <div>
+    <section>
       <Navbar />
-      <h1>OrdersDetails</h1>
-      <h2>
-        { id }
-        {' '}
-      </h2>
-    </div>
+      <p>{ details.totalPrice }</p>
+      <p>Detalhes do Pedido</p>
+      <p
+        data-testid="customer_order_details__element-order-details-label-order-id"
+      >
+        { `PEDIDO 000${id}` }
+      </p>
+      <p
+        data-testid="customer_order_details__element-order-details-label-seller-name"
+      >
+        P. Vend: Fulana Pereira
+      </p>
+      <p
+        data-testid="customer_order_details__element-order-details-label-order-date"
+      >
+        { `Date ${details.saleDate}` }
+      </p>
+      <p
+        data-testid={
+          `customer_order_details__element-order-details-label-delivery-status${id}`
+        }
+      >
+        { `Status ${details.status}` }
+      </p>
+      <button
+        type="button"
+        data-testid="customer_order_details__button-delivery-check"
+      >
+        MARCAR COMO ENTREGUE
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Valor Unitário</th>
+            <th>Subtotal</th>
+            <th>Remover Item</th>
+          </tr>
+        </thead>
+        <tbody>
+          { isSuccess && details.Products.map((item, i) => {
+            const { id: itemId, name, quantity, price, totalIten } = item;
+            return (
+              <tr key={ itemId }>
+                <td
+                  data-testid={
+                    `customer_order_details__element-order-table-item-number-${i}`
+                  }
+                >
+                  { i + 1 }
+                </td>
+                <td
+                  data-testid={ `customer_order_details__element-order-table-name-${i}` }
+                >
+                  { name }
+                </td>
+                <td
+                  data-testid={
+                    `customer_order_details__element-order-table-quantity-${i}`
+                  }
+                >
+                  { quantity }
+
+                </td>
+                <td
+                  data-testid={
+                    `customer_order_details__element-order-table-unit-price-${i}`
+                  }
+                >
+                  { price.replace('.', ',') }
+                </td>
+                <td
+                  data-testid={
+                    `customer_order_details__element-order-table-sub-total-${i}`
+                  }
+                >
+                  {totalIten.replace('.', ',')}
+                </td>
+              </tr>
+            );
+          }) }
+        </tbody>
+      </table>
+      <p>{ `Total: R$ ${details.totalPrice}` }</p>
+    </section>
   );
 }
